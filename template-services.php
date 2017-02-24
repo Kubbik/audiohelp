2508a8b8c067f57655d84435dd8eed75954eb424
Kubbik <shuklin_alexey@mail.ru>
2017-02-25 00:50:43 +0700

    page

---
+++ template-services.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Услуги
+ */
+// send contact
+if (isset($_POST['contact'])) {
+    $error = ale_send_contact($_POST['contact']);
+}
+get_header(); ?>
+
+    <div class="container contacts">
+        <div class="wrapper">
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <div class="page_content">
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; endif; ?>
+
+        </div>
+    </div>
+    <!--
+
+
+    <?php get_footer(); ?>
